1a64173721c05b055b241ffbfcdfaedaa32edf6c
megachickn101 <ryanshadow007@hotmail.com>
2022-02-04 17:34:18 -0500

    Added Symmetrical Encryption
    - 1.3
    - Symmetrical encryption
    - Auto-generates password files and Encryption keys on first startup.

--- Old Source Code/1.2/password-manager-win.py
+++ Old Source Code/1.2/password-manager-win.py
@@ -3,39 +3,10 @@ import time
 import pickle
 import random
 import os
-from cryptography.fernet import Fernet, MultiFernet
-
-def initial():
-	from cryptography.fernet import Fernet, MultiFernet
-	import pickle
-
-	key = Fernet.generate_key()
-	key2 = Fernet.generate_key()
-
-	pickle.dump(key, open( "1.p", "wb" ) )
-	pickle.dump(key2, open( "2.p", "wb" ) )
-
-	key = pickle.load( open( "1.p", "rb" ) )
-	key2 = pickle.load( open( "2.p", "rb" ) )
-
-	pickle.dump(False, open( "init.p", "wb" ) )
-	print("Keys Generated Successfully!")
-
-	fernet = Fernet(key)
-	default = "root"
-	endefault = fernet.encrypt(default.encode())
-	pickle.dump(endefault, open( "password.p", "wb" ) )
-
-	pickle.dump(True, open( "init.p", "wb" ) )
-	print("Password Created (Default: root)")
-	print("Please Restart This Program")
-	input()
 
 
 def read():
 	try:
-		key2 = pickle.load( open( "2.p", "rb" ) )
-		fernet = Fernet(key2)
 		conn = sqlite3.connect('pass.db')
 		c = conn.cursor()
 		print("What Service?:")
@@ -44,8 +15,8 @@ def read():
 		conn.commit()
 		y = c.fetchone()
 		Service = str(y[0])
-		Ename = fernet.decrypt(y[1]).decode()
-		Password = fernet.decrypt(y[2]).decode()
+		Ename = str(y[1])
+		Password = str(y[2])
 		print(f"Service: {Service}")
 		print(f"Username/Email: {Ename}")
 		print(f"Password: {Password}")
@@ -57,8 +28,6 @@ def read():
 		input()
 
 def write():
-	key2 = pickle.load( open( "2.p", "rb" ) )
-	fernet = Fernet(key2)
 	conn = sqlite3.connect('pass.db')
 	c = conn.cursor()
 	print("What Service Is This For? (This Will Be Used To Search For Your Info!)")
@@ -67,18 +36,16 @@ def write():
 	usermail_input = input()
 	print("And Finally, What Is You Password? (This Is Stored Only On Your Local Drive)")
 	password_input = input()
-	c.execute("INSERT INTO Info (Service, Username, Password) values (?, ?, ?)", (services_input, fernet.encrypt(usermail_input.encode()), fernet.encrypt(password_input.encode())))
+	c.execute("INSERT INTO Info (Service, Username, Password) values (?, ?, ?)", (services_input, usermail_input, password_input))
 	conn.commit()
 	conn.close()
 	print("Write Successful!")
 	input()
 
 def change():
-	key = pickle.load( open("1.p", "rb") )
-	fernet = Fernet(key)
 	initpassword = input("New Password: ")
-	pickle.dump(fernet.encrypt(initpassword.encode()), open( "password.p", "wb" ) )
-	print("Password changed!")
+	pickle.dump(initpassword, open( "password.p", "wb" ) )
+	print("password changed!")
 
 def gen():
 	adj = ["Crunchy", "Sweet", "Sexy", "Drugged", "Disgusting", "Mega", "Fat", "Skinny", "Fast", "Ripoff", "Intentional", "Illegal", "Brave", "Bad", "Good", "Awesome", "Uncool", "Cool", "Dying", "Dark", "Evil", "Dictating", "Rusty", "Big", "Sleepy", "Loser", "Ugly", "Flying", "Poopy", "Useless", "Hopeless", "Secure", "Unsecure", "Funny", "Slow", "Wonderful", "Disturbing", "Kool", "Happy", "Depressing", "Upset", "Sad", "Highest", "Fearful", "Lunatic", "Brilliant"]
@@ -136,25 +103,16 @@ def main():
 		input()
 
 
-#Execution
+pwd = pickle.load( open( "password.p", "rb" ) )
+pwd_in = input("Password?: ")
+if pwd == pwd_in:
+	main()
 
-init = pickle.load( open( "init.p", "rb" ) )
-if init == True:
-	key = pickle.load( open( "1.p", "rb" ) )
-	fernet = Fernet(key)
-	pwd = fernet.decrypt(pickle.load( open( "password.p", "rb" ) )).decode()
-	pwd_in = input("Password?: ")
-	if pwd == pwd_in:
-		os.system('clear')
-		main()
-
-	else:
-		print("incorrect password!")
-		while True:
-			input("Incorrect Password Relaunch To Try Again")
-
-	while True:
-		os.system('clear')
-		main()
 else:
-	initial()
+	print("incorrect password!")
+	while True:
+		input("Incorrect Password Relaunch To Try Again")
+
+while True:
+	os.system('cls')
+	main()
--- password-manager-win.py
+++ password-manager-win.py
@@ -3,10 +3,39 @@ import time
 import pickle
 import random
 import os
+from cryptography.fernet import Fernet, MultiFernet
+
+def initial():
+	from cryptography.fernet import Fernet, MultiFernet
+	import pickle
+
+	key = Fernet.generate_key()
+	key2 = Fernet.generate_key()
+
+	pickle.dump(key, open( "1.p", "wb" ) )
+	pickle.dump(key2, open( "2.p", "wb" ) )
+
+	key = pickle.load( open( "1.p", "rb" ) )
+	key2 = pickle.load( open( "2.p", "rb" ) )
+
+	pickle.dump(False, open( "init.p", "wb" ) )
+	print("Keys Generated Successfully!")
+
+	fernet = Fernet(key)
+	default = "root"
+	endefault = fernet.encrypt(default.encode())
+	pickle.dump(endefault, open( "password.p", "wb" ) )
+
+	pickle.dump(True, open( "init.p", "wb" ) )
+	print("Password Created (Default: root)")
+	print("Please Restart This Program")
+	input()
 
 
 def read():
 	try:
+		key2 = pickle.load( open( "2.p", "rb" ) )
+		fernet = Fernet(key2)
 		conn = sqlite3.connect('pass.db')
 		c = conn.cursor()
 		print("What Service?:")
@@ -15,8 +44,8 @@ def read():
 		conn.commit()
 		y = c.fetchone()
 		Service = str(y[0])
-		Ename = str(y[1])
-		Password = str(y[2])
+		Ename = fernet.decrypt(y[1]).decode()
+		Password = fernet.decrypt(y[2]).decode()
 		print(f"Service: {Service}")
 		print(f"Username/Email: {Ename}")
 		print(f"Password: {Password}")
@@ -28,6 +57,8 @@ def read():
 		input()
 
 def write():
+	key2 = pickle.load( open( "2.p", "rb" ) )
+	fernet = Fernet(key2)
 	conn = sqlite3.connect('pass.db')
 	c = conn.cursor()
 	print("What Service Is This For? (This Will Be Used To Search For Your Info!)")
@@ -36,16 +67,18 @@ def write():
 	usermail_input = input()
 	print("And Finally, What Is You Password? (This Is Stored Only On Your Local Drive)")
 	password_input = input()
-	c.execute("INSERT INTO Info (Service, Username, Password) values (?, ?, ?)", (services_input, usermail_input, password_input))
+	c.execute("INSERT INTO Info (Service, Username, Password) values (?, ?, ?)", (services_input, fernet.encrypt(usermail_input.encode()), fernet.encrypt(password_input.encode())))
 	conn.commit()
 	conn.close()
 	print("Write Successful!")
 	input()
 
 def change():
+	key = pickle.load( open("1.p", "rb") )
+	fernet = Fernet(key)
 	initpassword = input("New Password: ")
-	pickle.dump(initpassword, open( "password.p", "wb" ) )
-	print("password changed!")
+	pickle.dump(fernet.encrypt(initpassword.encode()), open( "password.p", "wb" ) )
+	print("Password changed!")
 
 def gen():
 	adj = ["Crunchy", "Sweet", "Sexy", "Drugged", "Disgusting", "Mega", "Fat", "Skinny", "Fast", "Ripoff", "Intentional", "Illegal", "Brave", "Bad", "Good", "Awesome", "Uncool", "Cool", "Dying", "Dark", "Evil", "Dictating", "Rusty", "Big", "Sleepy", "Loser", "Ugly", "Flying", "Poopy", "Useless", "Hopeless", "Secure", "Unsecure", "Funny", "Slow", "Wonderful", "Disturbing", "Kool", "Happy", "Depressing", "Upset", "Sad", "Highest", "Fearful", "Lunatic", "Brilliant"]
@@ -103,16 +136,25 @@ def main():
 		input()
 
 
-pwd = pickle.load( open( "password.p", "rb" ) )
-pwd_in = input("Password?: ")
-if pwd == pwd_in:
-	main()
+#Execution
 
-else:
-	print("incorrect password!")
+init = pickle.load( open( "init.p", "rb" ) )
+if init == True:
+	key = pickle.load( open( "1.p", "rb" ) )
+	fernet = Fernet(key)
+	pwd = fernet.decrypt(pickle.load( open( "password.p", "rb" ) )).decode()
+	pwd_in = input("Password?: ")
+	if pwd == pwd_in:
+		os.system('cls')
+		main()
+
+	else:
+		print("incorrect password!")
+		while True:
+			input("Incorrect Password Relaunch To Try Again")
+
 	while True:
-		input("Incorrect Password Relaunch To Try Again")
-
-while True:
-	os.system('cls')
-	main()
+		os.system('cls')
+		main()
+else:
+	initial()
